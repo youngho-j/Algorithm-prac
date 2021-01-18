e7a6f3fb9fe28d7bf2cb310212f32662c009a057
youngho-j <alfkwl1239@naver.com>
2021-01-18 17:08:44 +0900

    알고리즘 문자열 6 풀이완료 / countTokens() 메소드 사용 완료!

---
+++ Level1/src/main/String_6_1152.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class String_6_1152 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//		int cnt = 0;
+//		String str = null; 
+//		while(st.hasMoreTokens()) {
+//			str = st.nextToken();
+//			if(str != null) {
+//				cnt++;				
+//			}
+//		}
+//		System.out.println(cnt);
+		System.out.println(st.countTokens());
+	}
+}
+
+/*
+ * 참고사항 : StringTokenizer 클래스의 countTokens() 메소드 사용시 몇개의 토큰이 존재하는지 알 수 있음
+ */
