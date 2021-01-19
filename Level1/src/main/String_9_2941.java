ee9b74d2d6098ef356b62b71d67c1518c40574c2
youngho-j <alfkwl1239@naver.com>
2021-01-19 14:45:36 +0900

    알고리즘 문자열 8, 9 문제 풀이완료 / StringIndexOutBounds

---
+++ Level1/src/main/String_9_2941.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String_9_2941 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int len = str.length();
+		int count = 0;
+
+		for (int i = 0; i < len; i++) {
+			if (str.charAt(i) == 'c' && i < len - 1) {
+				if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
+					i++;
+				}
+			} else if (str.charAt(i) == 'd' && i < len - 1) {
+				if (str.charAt(i + 1) == '-') {
+					i++;
+				} else if (str.charAt(i + 1) == 'z' && i < len - 2) {
+					if (str.charAt(i + 2) == '=') {
+						i += 2;
+					}
+				}
+			} else if ((str.charAt(i) == 'l' || str.charAt(i) == 'n') && i < len - 1) {
+				if (str.charAt(i + 1) == 'j') {
+					i++;
+				}
+			} else if ((str.charAt(i) == 's' || str.charAt(i) == 'z') && i < len - 1) {
+				if (str.charAt(i + 1) == '=') {
+					i++;
+				}
+			}
+			count++;
+		}
+		System.out.println(count);
+	}
+}
+
+/*
+ * 원인 : StringIndexOutBounds - 문자열의 사이즈보다 큰 경우 발생
+ * 그 후 코드라인 줄이다가 오타로 인한('-' 구분하는 조건이 '=' 조건 두개였음... ㅠ) 실패
+ * 예제소스 디버깅했는데도 안나왔는데.. 오탈자 꼭 점검!
+ *
+ *
+ */
