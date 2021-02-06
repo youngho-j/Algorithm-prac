62356711f9a004458999cffe3f5e44dcb759056d
youngho-j <alfkwl1239@naver.com>
2021-02-06 16:50:18 +0900

    알고리즘 브루트포스 5 풀이완료! / String 클래스 contains 메소드

---
+++ BOJ/src/main/BruteForce_5_1436.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BruteForce_5_1436 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int num = 666;
+		int count = 1;
+
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")){
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+}
+/*
+ * String.contains("sd")
+ * 해당 문자열에 sd라는 문자열이 있는지 없는지 boolean값 리턴
+ */
