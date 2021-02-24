ef5e0d2664e70f07c1cfc2f5b6c6918130a4b7a5
youngho-j <alfkwl1239@naver.com>
2021-02-24 16:16:55 +0900

    알고리즘 백트래킹 6 스도쿠 다시 풀기 완료!
    point : 빈칸을 찾았을때 체크 후 재귀 혹은 되돌아가기

--- BOJ/src/main/Backtracking_6_2580.java
+++ BOJ/src/main/Backtracking_6_2580.java
@@ -7,20 +7,25 @@ import java.util.StringTokenizer;
 
 public class Backtracking_6_2580 {
 	public static int[][] sdokupan = new int[9][9];
-	
+//	빈 칸에 들어갈 값이 중복되는 경우 false를 리런함으로써 중복값이 없도록 검사
 	public static boolean checkCell(int row, int col, int value) {
+//		박스 3×3 조건의 가로 세로 시작값
 		int startHori = (row/3)*3;
 		int startVerti = (col/3)*3;
+		
+//		가로 조건
 		for(int i = 0; i < 9 ; i++) {
 			if(sdokupan[row][i] == value) {
 				return false;
 			}
 		}
+//		세로 조건
 		for(int j = 0; j < 9 ; j++) {
 			if(sdokupan[j][col] == value) {
 				return false;
 			}
 		}
+//		박스 3×3 조건
 		for(int k = startHori ; k < startHori + 3 ; k++) {
 			for(int l = startVerti ; l < startVerti + 3 ; l++) {
 				if(sdokupan[k][l] == value) {
@@ -46,18 +51,25 @@ public class Backtracking_6_2580 {
 				sb.append("\n");
 			}
 			System.out.println(sb);
+			
+/*			 첫번째로 나온 스도쿠판을 출력하고 강제 종료!
+ *			 System 클래스의 exit() 메소드를 사용하여 강제적으로 JVM을 종료 () 안에는 종료 상태값이 들어감
+ * 			 일반적으로 0은 정상종료 / 비정상종료는 0이외의 다른값
+ */
 			System.exit(0);
 		}
 		if(sdokupan[row][col] == 0) {
 //			1 ~ 9 중 들어갈 수 있는 값 찾기
 			for(int i = 1 ; i <= 9 ; i++) {
+				// 1 ~ 9 숫자 중 가능한 값인지 확인
 				if(checkCell(row, col, i)) {
+					// 가능한 경우 해당 값 대입
 					sdokupan[row][col] = i;
-//					다음 열의 값 찾기
+//					다음 열의 값 찾기(재귀)
 					sdoku(row, col + 1);
 				}
 			}
-//			빈칸이 많아 답을 못찾았을때 0으로 해 놓고 다른 것부터
+//			가능한 값이 없는 경우 0으로 변경 후 함수 종료
 			sdokupan[row][col] = 0;
 			return;
 		}
@@ -74,6 +86,7 @@ public class Backtracking_6_2580 {
 				sdokupan[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+//		처음 시작이 x : 0, y : 0이므로
 		sdoku(0, 0);
 	}
 }
