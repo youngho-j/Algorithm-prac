e151353196fe3c6b0153a6f22970bc59cccd7ea7
youngho-j <alfkwl1239@naver.com>
2021-05-07 13:09:13 +0900

    동적계획법 문제 가장 긴 바이토닉 수열 풀이완료!
    가장 긴 수열을 구하는 부분은 쉽게 해결했으나..
    작은 부분을 구할때 긴 수열을 구하고 작은 수열을 구하려다보니 삽질을 하게됨..
    쉽게 생각했다면 좀 더 빠르게 구현했을텐데 ㅠ
    가장 긴 증가 수열의 길이의 배열과 가장 긴 감소 수열의 길이를 합친후
    중복 되는 원소를 -1한 값을 출력함
    
    머리로 하기보단 일단 쓰고, 그리는게 훨씬 빠른듯

---
+++ BOJ/src/main/DynamicPrograming/DynamicPrograming_11_11054.java
@@ -0,0 +1,58 @@
+package main.DynamicPrograming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DynamicPrograming_11_11054 {
+	
+	private static int N;
+	private static int[] arr;
+	private static int[] dp;
+	private static int[] rDp;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		dp = new int[N];
+		rDp = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0 ; i < N ; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//LIS
+		for(int i = 0 ; i < N ; i++) {
+			dp[i] = 1;
+			for(int j = 0 ; j < i ; j++) {
+				if(arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		
+		//LDS
+		for(int i = N-1 ; i >= 0 ; i--) {
+			rDp[i] = 1;
+			for(int j = N-1 ; j > i ; j--) {
+				if(arr[j] < arr[i] && rDp[i] < rDp[j] + 1) {
+					rDp[i] = rDp[j] + 1;
+				}
+			}
+		}
+		
+		int max = 0;
+		for(int i = 0 ; i < N ; i++) {
+			if(max < dp[i] + rDp[i]) {
+				max = dp[i] + rDp[i];
+			}
+		}
+		//원소가 하나씩 중복되므로 
+		System.out.println(max -1);
+	}
+}
