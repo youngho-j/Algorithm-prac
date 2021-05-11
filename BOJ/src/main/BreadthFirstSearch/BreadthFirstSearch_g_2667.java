adf25c44d96a6c194cc4c5bb766f750d676f51e4
youngho-j <alfkwl1239@naver.com>
2021-05-11 23:57:45 +0900

    알고리즘 단지번호 붙이기 문제 풀이완료!
    시도 1
    컴파일 에러
    - 클래스 Main 이름 오타로 인한 오류
    
    시도 2
    NumberFormat 발생
    - 입력시 주어지는 값이 띄어쓰기가 있는 줄 알고 StringTokenizer 사용하여 에러
    
    시도 3
    오타로 인한 틀림
    - 입력 받은 값을 변환시 charAt() 메서드의 변수값 입력 오류로 발생
    
    시도 4
    오타로 인한 트림
    - 테스트 시 출력된 결과값이 연속적으로 이루어지지 않아 살펴보니 상하좌우 탐색시
    y값이 x로 오타 발생
    
    시도 5
    테스트 성공 후 정답 확인

--- BOJ/src/main/BreadthFirstSearch/BreadthFirstSearch_g_2667.java
+++ BOJ/src/main/BreadthFirstSearch/BreadthFirstSearch_g_2667.java
@@ -8,16 +8,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class BreadthFirstSearch_g_2667 {
-	
+	// 배열 크기, 단지내 세대수
 	private static int N;
 	private static int cnt;
-	
+	// 지도로 사용할 배열, 방문 여부 체크용 배열
 	private static int[][] map;
 	private static boolean[][] visited;
 	
-	private static int[] topBottom = {-1, 0, 1, 0};
-	private static int[] rightLeft = {0, 1, 0, -1};
+	// 상하 이동 배열, 좌우 이동 배열
+	private static int[] topBottom = {-1, 1, 0, 0};
+	private static int[] rightLeft = {0, 0, -1, 1};
 	
+	// 단지 수, 단지 내 세대 수 출력을 위한 배열
 	private static List<Integer> count;
 	
 	public static void main(String[] args) throws IOException{
@@ -28,22 +30,27 @@ public class BreadthFirstSearch_g_2667 {
 		map = new int[N][N];
 		visited = new boolean[N][N];
 		
+		// 주의할 점은 띄어 쓰기가 없으모로, charAt 메소드 사용 후 -'0'을 통해 int형으로 형변환
 		for(int i = 0 ; i < N ; i++) {
 			String temp = br.readLine();
 			for(int j = 0 ; j < N ; j++) {
-				map[i][j] = temp.charAt(i)-'0';
+				map[i][j] = temp.charAt(j)-'0';
 			}
 		}
 		
 		cnt = 0;
 		
 		count = new ArrayList<Integer>();
+		
+		// 전체 탐색
 		for(int i = 0 ; i < N ; i++) {
 			for(int j = 0 ; j < N ; j++) {
-				if(map[i][j] == 1) {
-					cnt = 1;
-					bfs(i, j);
-					count.add(cnt);
+				// map 배열의 값이 1이고, visited 배열의 값이 true일 경우(! 사용하여 false가 true) 
+				if(map[i][j] == 1 && !visited[i][j]) {
+					// 단지의 첫 시작이므로 +1 후 탐색
+					cnt = 1; 
+					bfs(i, j); 
+					count.add(cnt); 
 				}
 			}
 		}
@@ -55,13 +62,14 @@ public class BreadthFirstSearch_g_2667 {
 		}
 		System.out.println(sb);
 	}
-	
+	// 해당 세대의 상하좌우 탐색
 	public static void bfs(int x, int y) {
+		// 방문시 해당 값을 false에서 true로 변경, 조건에서 걸러지게됨
 		visited[x][y] = true;
 		
 		for(int i = 0 ; i < 4 ; i++) {
 			int calX = x + topBottom[i];
-			int calY = x + rightLeft[i];
+			int calY = y + rightLeft[i];
 			// map 범위안에서 
 			if(calX >= 0 && calY >= 0 && calX < N && calY < N) {
 				if(map[calX][calY] == 1 && !visited[calX][calY]) {
